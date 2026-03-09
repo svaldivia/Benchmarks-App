d078bcc167635cb641f9a1fc007d361dd555fcd6
svaldivia <sebastianvaldivia@gmail.com>
2026-03-08 22:28:00 -0700

    Adding keys to env file

--- data/firebase/firebaseConfig.ts
+++ data/firebase/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOzZsLX1BYviuwABFOSUn9KB3JUw-3nwc',
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'fit-benchmark-app.firebaseapp.com',
   projectId: 'fit-benchmark-app',
   storageBucket: 'fit-benchmark-app.firebasestorage.app',
-  messagingSenderId: '940140176485',
-  appId: '1:940140176485:web:dcc024aeb5cca37f26928c',
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
